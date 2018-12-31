545784ff8646cdbbb91e9d7d04166c5bdbe8c85b
Steven Foster <stevenscottfoster@icloud.com>
2018-12-31 12:26:16 -0800

    added default route example

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # Simple route
   get 'demo/index'
+
+  # Default Route
+  #get ':controller(/:action(/:id))'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
