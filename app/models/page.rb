70bf3098174a3d8afe0bc5de7697ec509d83cd7d
Steven Foster <stevenscottfoster@icloud.com>
2019-01-01 15:29:30 -0800

    added additional validations to admin user page section and subject models

--- app/models/page.rb
+++ app/models/page.rb
@@ -11,5 +11,9 @@ class Page < ApplicationRecord
     scope :search, lambda { |query| where(["name LIKE ?", "%#{query}"])}
 
     validates_presence_of :name
+    validates_length_of :name, :maximum => 255
+    validates_presence_of :permalink
+    validates_length_of :name, :within => 3..255
+    validates_uniqueness_of :permalink
 
 end
