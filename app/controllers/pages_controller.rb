2660c169e543b839ec598e16724e51f7818f64f6
Steven Foster <stevenscottfoster@icloud.com>
2019-01-01 11:59:53 -0800

    updated pages controller with all methods for CRUD actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,50 @@
 class PagesController < ApplicationController
+
   def index
+    @pages = Page.sorted
   end
 
   def show
+    @page = Page.find(params[:id])
   end
 
   def new
+    @page =Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+    if @page.save
+      flash[:notice] = "Page Created Successfully."
+      redirect_to(pages_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update_attributes(page_params)
+      flash[:notice] = "Page Updated Successfully."
+      redirect_to(page_path(@page))
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @page = Page.find(params[:id])
   end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    flash[:notice] = "Page Destroyed Successfully."
+    redirect_to(pages_path)
+  end
+
 end
